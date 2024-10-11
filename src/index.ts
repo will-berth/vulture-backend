d29a2c0c7ba71ec1951c979c71aa2feedf16336e
WilberthLP <wilberth@syklo.io>
2024-10-11 17:02:38 -0600

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import { router as productRouter } from './routes/productsRouter';
 import { router as saleRouter } from './routes/salesRouter';
@@ -9,6 +10,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use('/api/products', productRouter);
 app.use('/api/sales', saleRouter);
